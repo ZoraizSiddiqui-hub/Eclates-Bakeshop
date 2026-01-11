5ed22cc2c8d84f7c6d720feaa6169e42480b985f
ZoraizSiddiqui-hub <127402292+ZoraizSiddiqui-hub@users.noreply.github.com>
2026-01-11 05:35:44 +0500

    Update MongoDB connection string with new credentials

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 
 export const connectDB = async () => {
-        await mongoose.connect ('mongodb+srv://zoraizsid49:BBBanchoss1122@cluster0.7hn88hg.mongodb.net/Eclates').then(() => {
+        await mongoose.connect ('mongodb+srv://zoraizsiddiqui:zoraizsiddiqui@cluster0.7hn88hg.mongodb.net/Eclates').then(() => {
             console.log("MongoDB connected successfully");
         })
     }                
